c3de23faba32a4280da0715ca6a4f00ef2471e5b
abonec <abonec@gmail.com>
2013-08-24 23:34:00 +0400

    @= test vk on heroku

--- app.rb
+++ app.rb
@@ -13,3 +13,8 @@ end
 get '/links' do
   haml :links, locals: { series: get_series(params[:url]) }
 end
+
+require 'open-uri'
+get '/heroku_test' do
+  open 'http://vk.com/video_ext.php?oid=-56941034&id=165774609&hash=328e1a94b8d1bfff&sd'
+end
