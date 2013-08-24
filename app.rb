8ecfcee6ec672b179a642874445f25462a2b3d70
abonec <abonec@gmail.com>
2013-08-24 21:59:01 +0400

    @= fix =)

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ puts get_series("http://amovies.tv/serials/490-vo-vse-tyazhkie.html")
 
 
 get '/' do
-  "<a src='http://cs1-50v4.vk.me/p19/cded410d8122.720.mp4'>http://cs1-50v4.vk.me/p19/cded410d8122.720.mp4</a>"
+  "<a href='http://cs1-50v4.vk.me/p19/cded410d8122.720.mp4'>http://cs1-50v4.vk.me/p19/cded410d8122.720.mp4</a>"
 end
 
 
