0cdf516da8edb741df55807528f4533f60887bd4
abonec <abonec@gmail.com>
2013-08-25 12:03:00 +0400

    @= test head request for cdn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'haml'
 require './parser'
+require 'net/http'
 
 include Parser
 
@@ -17,4 +18,10 @@ end
 require 'open-uri'
 get '/heroku_test' do
   open 'http://vk.com/video_ext.php?oid=-56941034&id=165774609&hash=328e1a94b8d1bfff&sd'
+end
+
+get '/vk_head' do
+  uri = URI.parse "http://cs533322v4.vk.me/u219120229/videos/a26f0fd78c.720.mp4"
+  req = Net::HTTP.new(uri.hostname, 80)
+  req.request_head(uri.path).inspect
 end
