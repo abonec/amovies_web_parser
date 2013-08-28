9772a6cc45e8be41fd2a9e339683dc7aec1b0508
abonec <abonec@gmail.com>
2013-08-28 23:32:36 +0400

    @= remove old shit

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require './parser'
 require 'net/http'
 
 include Parser
+set :bind, '0.0.0.0'
+set :port, '6100'
 
 set :public_folder, 'public'
 
@@ -13,15 +15,4 @@ end
 
 get '/links' do
   haml :links, locals: { series: get_series(params[:url]) }
-end
-
-require 'open-uri'
-get '/heroku_test' do
-  open 'http://vk.com/video_ext.php?oid=-56941034&id=165774609&hash=328e1a94b8d1bfff&sd'
-end
-
-get '/vk_head' do
-  uri = URI.parse Parser.parse_vk("http://vk.com/video_ext.php?oid=-56436575&id=165929277&hash=225039cd9b76e1e9&sd").to_a.last.last
-  req = Net::HTTP.new(uri.hostname, 80)
-  req.request_head(uri.path).inspect
 end
--- parser.rb
+++ parser.rb
@@ -9,13 +9,15 @@ module Parser
     Hash[value.split("&").select{|param| param[/^url\d\d\d\=/]}.map do |param|
       param.match(/^url(\d\d\d)\=(.*)/).captures
     end]
+  rescue
+    {}
   end
 
   def parse_amovies url
     doc = Nokogiri::HTML open url
 
     result = Hash[doc.xpath('//select[@id="series"]/option').map{|option| [option.content.force_encoding("utf-8"), option.attributes["value"].to_s]}]
-    result[:title] = doc.css('.page .full-news center b').first.content
+    result[:title] = doc.css('.title_d_dot span').first.content
     result
   end
 
